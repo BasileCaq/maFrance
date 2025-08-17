2930e29ff645411b0dc3d65bb123cab42508ebe2
BasileCaq <basilecaquot@hotmail.fr>
2025-08-17 12:22:32 +0200

    J'ai réparé

--- main.js
+++ main.js
@@ -192,6 +192,13 @@ function afficherListeCommunes(communes) {
   });
 }
 
+function newListDepartement(nom, numéro, nb_commune){
+      const item_dep = document.createElement('div');
+      item_dep.textContent = nom;
+      communeList.appendChild(item_dep);
+      return  item_dep;
+}
+
 
 async function chercherVisitesParNom(nomRecherche) {
 const { data, error } = await supabase
